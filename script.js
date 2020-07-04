b0867310333a1a340f1e9c2d0a69b5cae8e69e47
Michele <michele.carmagnani@gmail.com>
2020-07-04 10:49:40 +0200

    text of the page

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const taggle = document.getElementById("toggle");
 const closeDown = document.getElementById("close");
 const openUp = document.getElementById("open");
 const modal = document.getElementById("modal");
-
+const wrapper = document.getElementById("wrapper");
+const body = document.querySelector("body");
 // toggle nav
 
 toggle.addEventListener("click", () => {
@@ -25,3 +26,11 @@ closeDown.addEventListener("click", () => {
 window.addEventListener("click", (e) =>
   e.target == modal ? modal.classList.remove("show-modal") : false
 );
+
+window.addEventListener("click", (e) => {
+  console.log(body);
+  if (e.target == body &&  modal.className == "show-modal") {
+   
+    modal.classList.remove("show-modal");
+  }
+});
